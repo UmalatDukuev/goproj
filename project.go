d036cb0ef904d6b4bb03185fc7b0bd60c8fb2d41
Umalat <dukuev037@mail.ru>
2024-08-29 22:17:06 +0300

    profile

---
+++ project.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	//fmt.Println("hello")
+	//var age int = 12
+	//fmt.Println(age)
+	//const pi = 3.14
+	var web string = "111"
+	fmt.Println(web + "12")
+}
