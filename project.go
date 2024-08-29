5a12dd6b7d067ab9d9c446d7c22be3150ebc0150
Umalat <dukuev037@mail.ru>
2024-08-29 22:32:42 +0300

    profile

--- project.go
+++ project.go
@@ -3,10 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	//fmt.Println("hello")
-	//var age int = 12
-	//fmt.Println(age)
-	//const pi = 3.14
-	var web string = "111"
-	fmt.Println(web + "12")
+
+	websites := make(map[string]int)
+	websites["itProger"] = 4
+	websites["Yandex"] = 5
+	fmt.Println(websites["itProger"])
 }
