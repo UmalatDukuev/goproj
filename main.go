ffe4793f462d871a6a3ae6f63901d547ef4bb80e
u.dukuev <dukuev037@mail.ru>
2024-09-05 23:38:58 +0300

    1

--- main.go
+++ main.go
@@ -13,9 +13,14 @@ func contact_page(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Coooooooooontacts")
 }
 
+func friends_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Frieeeeeeeeeends")
+}
+
 func handleRequest() {
 	http.HandleFunc("/", home_page)
 	http.HandleFunc("/contacts", contact_page)
+	http.HandleFunc("/friends", friends_page)
 	http.ListenAndServe(":8080", nil)
 }
 
