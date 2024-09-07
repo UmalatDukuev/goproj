ada5d279b32a825c163b044f467b8dfbe9866a07
Umalat <dukuev037@mail.ru>
2024-09-07 15:08:29 +0300

    1

--- bd.go
+++ bd.go
@@ -15,6 +15,11 @@ const (
 	dbname   = "users"
 )
 
+type User struct {
+	Name string `json:"name"`
+	Age  uint16 `json:"age"`
+}
+
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
@@ -25,10 +30,27 @@ func main() {
 	}
 	defer db.Close()
 
-	// err = db.Ping()
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	// insert, err := db.Query(`INSERT INTO "users" ("name", "age") VALUES('Alexxxx', '12')`)
 	// if err != nil {
 	// 	panic(err)
 	// }
+	// defer insert.Close()
 
+	res, err := db.Query("SELECT name, age FROM users")
+
+	for res.Next() {
+		var user User
+		err = res.Scan(&user.Name, &user.Age)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(fmt.Sprintf("User: %s with age %d", user.Name, user.Age))
+		//fmt.Println(1)
+	}
 	fmt.Println("Successfully connected!")
 }
