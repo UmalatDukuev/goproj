b808b5341a6a51afd49d87158ffa20ab34cbb026
u.dukuev <dukuev037@mail.ru>
2024-09-12 18:42:47 +0300

    1

--- www/main.go
+++ www/main.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-	_ "fmt"
+	"fmt"
 	"html/template"
 	"net/http"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-	// t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
-	// if err != nil {
-	// 	fmt.Fprintf(w, err.Error())
-	// }
-	// t.ExecuteTemplate(w, "index", nil)
-	templ, _ := template.ParseFiles("1.html")
-	templ.Execute(w, 1)
+	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	t.ExecuteTemplate(w, "index", nil)
+	// templ, _ := template.ParseFiles("templates/index.html")
+	// templ.Execute(w, 1)
 }
 
 func handleFunc() {
-	//http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 	http.HandleFunc("/", index)
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8030", nil)
 }
 
 func main() {
