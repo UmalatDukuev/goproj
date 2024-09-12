62eb9ffedba8db4201df54bf1ecf65b91f78534c
u.dukuev <dukuev037@mail.ru>
2024-09-12 20:22:08 +0300

    1

--- www/main.go
+++ www/main.go
@@ -12,15 +12,30 @@ func index(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, err.Error())
 	}
 	t.ExecuteTemplate(w, "index", nil)
-	// templ, _ := template.ParseFiles("templates/index.html")
-	// templ.Execute(w, 1)
+}
+
+func create(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/create.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	t.ExecuteTemplate(w, "create", nil)
+}
+
+func save_article(w http.ResponseWriter, r *http.Request) {
+	title := r.FormValue("title")
+	anons := r.FormValue("anons")
+	full_text := r.FormValue("full_text")
+
 }
 
 func handleFunc() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 	http.HandleFunc("/", index)
+	http.HandleFunc("/create", create)
+	http.HandleFunc("/save_article", save_article)
 
-	http.ListenAndServe(":8030", nil)
+	http.ListenAndServe(":8080", nil)
 }
 
 func main() {
