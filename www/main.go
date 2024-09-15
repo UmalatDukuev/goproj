c41abed758930d4a9bd6ccd698d23e1bd0bbd447
Umalat <dukuev037@mail.ru>
2024-09-15 12:32:19 +0300

    рабочая версия

--- www/main.go
+++ www/main.go
@@ -17,12 +17,45 @@ const (
 	dbname   = "users"
 )
 
+type Article struct {
+	Id                     uint16
+	Title, Anons, FullText string
+}
+
+var posts = []Article{}
+
 func index(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
 	}
-	t.ExecuteTemplate(w, "index", nil)
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	insert, err := db.Query("SELECT * FROM articles")
+	if err != nil {
+		panic(err)
+	}
+	posts = []Article{}
+	for insert.Next() {
+		var post Article
+		err = insert.Scan(&post.Id, &post.Title, &post.Anons, &post.FullText)
+		if err != nil {
+			panic(err)
+		}
+		//fmt.Println((fmt.Sprintf("Post: %s with id: %d", post.Title, post.Id)))
+		posts = append(posts, post)
+	}
+
+	defer insert.Close()
+
+	t.ExecuteTemplate(w, "index", posts)
 }
 
 func create(w http.ResponseWriter, r *http.Request) {
@@ -38,21 +71,25 @@ func save_article(w http.ResponseWriter, r *http.Request) {
 	anons := r.FormValue("anons")
 	full_text := r.FormValue("full_text")
 
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
+	if title == "" || anons == "" || full_text == "" {
+		fmt.Fprintf(w, "Не все данные заполнены")
+	} else {
+		psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+			"password=%s dbname=%s sslmode=disable",
+			host, port, user, password, dbname)
+		db, err := sql.Open("postgres", psqlInfo)
+		if err != nil {
+			panic(err)
+		}
+		defer db.Close()
 
-	insert, err := db.Query("INSERT INTO articles (title, anons, full_text) VALUES ($1, $2, $3)", title, anons, full_text)
-	if err != nil {
-		panic(err)
+		insert, err := db.Query("INSERT INTO articles (title, anons, full_text) VALUES ($1, $2, $3)", title, anons, full_text)
+		if err != nil {
+			panic(err)
+		}
+		defer insert.Close()
+		http.Redirect(w, r, "/", http.StatusSeeOther)
 	}
-	defer insert.Close()
-	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func handleFunc() {
@@ -60,7 +97,7 @@ func handleFunc() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/create", create)
 	http.HandleFunc("/save_article", save_article)
-	http.ListenAndServe(":8085", nil)
+	http.ListenAndServe(":8081", nil)
 }
 
 func main() {
